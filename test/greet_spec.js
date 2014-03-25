61f392ba26132ef1a3fae51c8fd462449aa4a76c
kaeli <puiesabu@gmail.com>
2014-03-25 17:20:06 +0800

    Bonus: use "should" syntax instead of "expect"

--- test/greet_spec.js
+++ test/greet_spec.js
@@ -9,3 +9,18 @@ describe('greet', function(){
     expect(greet("kaeli", true)).to.eql("hello kaeli, you look sexy today");
   });
 });
+
+var chai = require("chai");
+chai.should();
+
+describe('greet with "should" syntax', function() {
+  it("should greet a person by name", function() {
+  	result = greet("kaeli");
+  	result.should.equal("hello, kaeli")
+  });
+
+  it("should greet a person flirtatiously if drunk", function() {
+  	result = greet("kaeli", true);
+  	result.should.equal("hello kaeli, you look sexy today")
+  });
+});
