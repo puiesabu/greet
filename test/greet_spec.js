e1a4669bb83034ed24e5d9e178f2e9cf1cc44966
kaeli <puiesabu@gmail.com>
2014-03-25 10:15:56 +0800

    Implement mocha-first-test

---
+++ test/greet_spec.js
@@ -0,0 +1,11 @@
+var expect = require("chai").expect
+var greet = require("greet")
+describe('greet', function(){
+  it("should greet a person by name", function() {
+    expect(greet("kaeli")).to.eql("hello, kaeli");
+  });
+
+  it("should greet a person flirtatiously if drunk", function() {
+    expect(greet("kaeli", true)).to.eql("hello kaeli, you look sexy today");
+  });
+});
